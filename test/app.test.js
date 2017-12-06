f216f55793a7800be6ef424489844b716ccc0540
Faduma <faduma.mohammed@gmail.com>
2017-12-06 20:03:16 +0000

    feat: testing added and circli config

---
+++ test/app.test.js
@@ -0,0 +1,9 @@
+const request = require('supertest');
+const app = require('../server/app')
+
+describe('Test the root path', () => {
+    test('It should be an Unauthorized request', async () => {
+        const response = await request(app).get('/');
+        expect(response.statusCode).toBe(401);
+    });
+})
