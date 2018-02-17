75c12b341b2bf40e651f227bce96d45d7ac6e17b
NOAH <noah@reinvention.io>
2018-02-18 02:49:59 +0700

    test: Add beginning of User Integration test

--- test/app.test.js
+++ test/app.test.js
@@ -1,44 +1,9 @@
 const request = require('supertest');
 const app = require('server/app')
-const mongoose = require('mongoose');
-const User = require(__root + 'components/user/model');
-const createToken = require(__root + 'test/utils/createToken');
-
-function dropDB(db = mongoose) {
-	if (!mongoose.connection) {
-		throw Error('Missing database connection');
-	}
-	return mongoose.connection.dropDatabase();
-}
 
 describe('Root Path', () => {
-	test('It should be an Unauthorized request', async () => {
-		const response = await request(app)
-			.get('/');
-
-		await dropDB();
+	test('It should be an Unauthorized Request', async () => {
+		const response = await request(app).get('/');
 		await expect(response.statusCode).toBe(401);
 	});
-})
-
-describe('Return User', () => {
-	test('It should return user', async () => {
-		const user = new User({
-				givenName: 'narp',
-				email: 'narp@gmail.com',
-		});
-		user.save();
-
-		const token = createToken(user);
-		const response = await request(app)
-			.get(`/user/${user.id}`)
-			.set('Authorization', 'Bearer ' + token);
-		
-		await dropDB();
-		await expect(JSON.parse(response.text)["_id"]).toBe(user.id);
-	});
-})
-
-// mongoose.connect(process.env.MONGODB_TEST, function(err) {
-//     mongoose.connection.db.dropDatabase();
-// });
+});
---
+++ test/integration/users.test.js
@@ -0,0 +1,38 @@
+const request = require('supertest');
+const app = require('server/app')
+const User = require(__root + 'components/user/model');
+const createToken = require(__root + 'test/utils/createToken');
+
+describe('Integration: User', () => {
+  test('GET /user/:id with JWT token should return correct user', async () => {
+    const user = await new User({
+        givenName: 'narp',
+        email: 'narp@gmail.com',
+    });
+    user.save();
+    const token = createToken(user);
+
+    const response = await request(app)
+      .get(`/user/${user.id}`)
+      .set('Authorization', 'Bearer ' + token);
+
+    await expect(JSON.parse(response.text)["_id"]).toBe(user.id);
+    await User.remove(user);
+  });
+
+  test('GET /user/:id without a JWT token be an Unauthorized Request', async () => {
+    const user = await new User({
+        givenName: 'narp',
+        email: 'narp@gmail.com',
+    });
+    user.save();
+    // const token = createToken(user);
+
+    const response = await request(app)
+      .get(`/user/${user.id}`)
+      // .set('Authorization', 'Bearer ' + token);
+
+    await expect(response.statusCode).toBe(401);
+    await User.remove(user);
+  });
+});
