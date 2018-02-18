a24a219860a8858eb6ef675a97a5a6d14ac54a83
NOAH <noah@reinvention.io>
2018-02-18 21:53:53 +0700

    test: Add facebook JWT token test

--- test/integration/authentication.test.js
+++ test/integration/authentication.test.js
@@ -2,18 +2,44 @@ const request = require('supertest');
 const app = require('server/app');
 const mockUserFromFacebook = require(__root + 'test/fixtures/mockUserFromFacebook');
 const User = require(__root + 'components/user/model');
-const createToken = require(__root + 'test/utils/createToken');
 
+const JWT = require('jsonwebtoken')
 describe('Social Authentication', () => {
-  test('POST /auth/facebook creates a new user with facebook id attached', async () => {
+  test('POST /auth/facebook : if no user is present it creates a new user with facebook id attached, and returns JWT Token', async () => {
+    const mockUser = Object.assign({}, mockUserFromFacebook);
     const response = await request(app)
       .post('/auth/facebook')
-      .send(mockUserFromFacebook)
+      .send(mockUser)
 
-    const user = await User.find({ givenName: mockUserFromFacebook.firstName });
+    const responseToken = JSON.parse(response.text)["token"];
+    const userArray = await User.find({ givenName: mockUser.firstName });
+    const user = userArray[0];
 
-    await expect(mockUserFromFacebook.id).toEqual(user[0].facebook.id);
+    await expect(responseToken).toBeTruthy()
+    await expect(mockUser.id).toEqual(user.facebook.id);
+    await User.remove(user);
   });
 
+  test('POST /auth/facebook : if a user with the same facebook id is present it returns the JWT Token', async () => {
+    const mockUser = Object.assign({}, mockUserFromFacebook);
+    const user = await new User({
+      email: mockUser.email,
+      givenName: mockUser.givenName,
+      familyName: mockUser.familyName,
+      facebook: {
+        id: mockUser.id, 
+      }
+    });
+    const response = await request(app)
+      .post('/auth/facebook')
+      .send(mockUser);
+    const responseToken = JSON.parse(response.text)["token"];
+
+    await expect(responseToken).toBeTruthy();
+    await User.remove(user);
+  });
+
+  // test('POST /auth/facebook return' )
+    // const responseToken =;
   // test('')
 });
