fd9e12f4bc02cc79232f4145cac23d662e9f0ece
NOAH <noah@reinvention.io>
2018-02-22 19:42:14 +0700

    chore: Update database config

--- config/db.js
+++ config/db.js
@@ -2,8 +2,8 @@ require('dotenv').config();
 const mongoose = require('mongoose');
 
 const databaseOptions = {
-  test: process.env.MONGODB_TEST, 
-  development: process.env.MONGODB_LOCAL,
+  test: process.env.MONGOD_TEST, 
+  development: process.env.MONGODB,
 };
 
 const databaseUri = databaseOptions[process.env.NODE_ENV];
