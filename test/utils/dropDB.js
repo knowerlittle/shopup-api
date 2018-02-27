e1dfb4c50bfd093be76a8a1525746ea2b9a0556c
NOAH <noah@reinvention.io>
2018-02-28 00:02:54 +0700

    chore: Add --runInBand for Jest tests in npm run test

--- test/utils/dropDB.js
+++ test/utils/dropDB.js
@@ -1,6 +1,9 @@
 const mongoose = require('mongoose');
 
-const dropDB = async (collection) => await mongoose.connection.collections[collection].drop();
+const dropDB = async (collection) => (
+  await mongoose.connection.collections[collection].drop(async() => {
+    await console.log(`${collection} dropped`);
+  }));
 
 
 module.exports = dropDB;
