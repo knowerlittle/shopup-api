24e3c2264ffa8f6ad55cbf1b6997bf1db56644ef
NOAH <noah@reinvention.io>
2018-03-08 17:38:39 +0700

    feat: Create space model

---
+++ services/space/model.js
@@ -0,0 +1,63 @@
+const mongoose = require('mongoose');
+
+const spaceSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  profileViews: {
+    type: Number,
+    default: 0,
+  },
+  published: {
+    type: Boolean,
+    default: false,
+  },
+  stripeId: {
+    type: String,
+  },
+  socialLinks: {
+    type: Object,
+  },
+  users: {
+    type: mongoose.Schema.ObjectId,
+    ref: 'User',
+  },
+  categories: {
+    type: Array,
+    required: true,
+  },
+  demographics: {
+    type: Array,
+    required: true,
+  },
+  cities: {
+    type: Array,
+  },
+  bookings: {
+    type: Array,
+  },
+  chatrooms: {
+    type: Array,
+  },
+  cards: {
+    type: Array,
+  },
+  reviews: {
+    type: Array,
+  },
+  video: {
+    type: String,
+  },
+  images: {
+    type: Array,
+  },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Space', spaceSchema);
---
+++ test/unit/space.test.js
@@ -0,0 +1,34 @@
+const request = require('supertest');
+const app = require('server/app');
+const dropDB = require(__root + 'test/utils/dropDB');
+const Space = require(__root + 'services/space/model');
+const User = require(__root + 'services/user/model');
+const space1 = require(__root + 'test/fixtures/space1');
+
+const SPACES = 'spaces';
+const USERS = 'users';
+
+describe('Unit: Space', () => {
+  test('has correct required fields', async done => {
+    const user = await new User({
+      givenName: 'testUesr',
+      email: 'test@testing.com',
+    });
+    await user.save();
+
+    const spaceInfoWithUserId = Object.assign({}, space1, { users: user.id });
+
+    const space = await new Space(spaceInfoWithUserId);
+    await space.save();
+
+    await expect(space.name).toEqual('test space');
+    await expect(space.description).toEqual('this is a good space');
+    await expect(space.users.toString()).toEqual(user.id);
+    await expect(space.categories.length).toEqual(3);
+    await expect(space.demographics.length).toEqual(3);
+    await expect(space.cities.length).toEqual(2);
+    await dropDB(SPACES);
+    await dropDB(USERS);
+    await done();
+  });
+});
